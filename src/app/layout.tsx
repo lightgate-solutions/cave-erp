cbef7df53325501cfbcdd37181c03defe780379d
Elameen Daiyabu <elameendaiyabu@proton.me>
2025-12-11 16:09:56 +0100

    added vercel speed insight

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import NextTopLoader from "nextjs-toploader";
 import QueryProvider from "@/components/providers/query-provider";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -36,6 +37,7 @@ export default function RootLayout({
             <div>
               {children}
               <Analytics mode="production" />
+              <SpeedInsights />
             </div>
           </QueryProvider>
           <SonnerToaster richColors position="top-center" />
